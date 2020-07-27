6f40b152db11f241d43fe6229d2cac4e91653e1a
Wolf090685 <dumarenok@tut.by>
2020-07-27 16:47:24 +0300

    after 1 day

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+// Импортирование модулей
+
+import { radioPlayerInit } from './radioPlayer.js';
+import { musicPlayerInit } from './musicPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
+
+// Объявление переменных
+
+const playerBtn = document.querySelectorAll('.player-btn'),
+      playerBlock = document.querySelectorAll('.player-block'),
+      temp = document.querySelector('.temp');
+
+
+// Деактивация плееров
+
+const deactivationPlayer = () => {
+    temp.style.display = 'none';    
+    playerBtn.forEach(item => {
+        item.classList.remove('active');
+    });
+    playerBlock.forEach(item => {
+        item.classList.remove('active');
+    });
+}
+
+// Tabs
+
+playerBtn.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        deactivationPlayer();
+        btn.classList.add('active');
+        playerBlock[i].classList.add('active');
+    });
+});
+
+// Вызов функций   
+
+radioPlayerInit();
+musicPlayerInit();
+videoPlayerInit();
